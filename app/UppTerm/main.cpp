abd6fb94219703ec2a42872e5e532932c0c13ef5
İsmail Yılmaz <32938453+ismail-yilmaz@users.noreply.github.com>
2023-12-16 21:50:48 +0300

    UppTerm: Code refactored. (#2)

--- app/UppTerm/main.cpp
+++ app/UppTerm/main.cpp
@@ -15,31 +15,21 @@ const char *tshell = "ComSpec"; // Alternatively you can use powershell...
 
 class UppTerm final : TopWindow {
 public:
-	UppTerm() {
+	UppTerm()
+	{
 		Title(m_title_prefix);
 		Icon(UppTermImg::Icon());
 		SetRect(m_terminal.GetStdSize());
 		Sizeable().Zoomable().CenterScreen().Add(m_terminal.SizePos());
 		
 		m_terminal.WindowReports();
-		m_terminal.WhenTitle = [=](String s) { Title(m_title_prefix + " :: " + s); };
+		m_terminal.WhenTitle  = [=](String s) { Title(m_title_prefix + " :: " + s); };
+		m_terminal.WhenOutput = [=](String s) { m_pty.Write(s); };
+		m_terminal.WhenResize = [=]()         {	m_pty.SetSize(m_terminal.GetPageSize()); };
 	}
-	
-	void Close() override {
-		if (m_pty.IsRunning()) {
-			m_pty.Kill();
-		}
-	}
-	
+
 	int Run(const String& cmd)
 	{
-		m_terminal.WhenOutput = [&](String s) {
-			m_pty.Write(s);
-		};
-		m_terminal.WhenResize = [&]() {
-			m_pty.SetSize(m_terminal.GetPageSize());
-		};
-		
 		m_pty.Start(cmd.IsEmpty() ? GetEnv(tshell) : cmd, Environment(), GetHomeDirectory());
 		
 		OpenMain();
@@ -49,13 +39,12 @@ public:
 			String s = m_pty.Get();
 			int l = s.GetLength();
 			m_terminal.WriteUtf8(s);
-			if(!m_pty.IsRunning())
+			if(!m_pty.IsRunning() || !IsOpen())
 				break;
 			Sleep(l >= 1024 ? 1024 * 10 / l : 10);
 		}
-		const auto exit_code = m_pty.GetExitCode();
-		m_pty.Kill();
-		return exit_code;
+		
+		return m_pty.GetExitCode();
 	}
 
 private:
@@ -78,3 +67,4 @@ GUI_APP_MAIN
 	
 	SetExitCode(UppTerm().Run(cmd));
 }
+
